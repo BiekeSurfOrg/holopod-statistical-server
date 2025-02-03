aa070241884e32121e1b22c77a2da863d47a7355
BorisStankov98 <boris.stankovv@gmail.com>
2025-02-03 16:56:23 +0200

    test rout

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/holopod-content'));
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname +
-    '/dist/holopod-content/index.html'));
+app.get('/test', function (req, res) {
+  res.send('Hello World');
 });
 
 console.log("app running");
